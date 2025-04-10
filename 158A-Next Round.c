8725a0f3dc07f83f076923cfef969c9db154c993
mahfujur-r <rahmanmahfujur0701@gmail.com>
2025-04-10 20:30:15 +0600

    Create 158A-Next Round.c

---
+++ 158A-Next Round.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int a[50];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int cutoff = a[k - 1];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= cutoff && a[i] > 0) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
